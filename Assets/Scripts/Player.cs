d80239cb2716cb45abbab3c72af63a518cbed053
KevinDu10 <kevinwd2401@gmail.com>
2026-01-27 04:10:35 -0500

    Player setup

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public bool worldActive;
+    public Camera cam;
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void TogglePlayer(bool active) {
+        worldActive = active;
+        GetComponent<Collider>().enabled = worldActive;
+    }
+
+    public void Movement(Vector3 velocity) {
+        rb.AddForce(velocity, ForceMode.Acceleration);
+    }
+
+    public bool CheckCollision() {
+        // return true if there is collision
+        return Physics.CheckSphere(transform.position, 0.52f, 1 << 6, QueryTriggerInteraction.Ignore);
+    }
+
+    public void DeathAnimation() {
+
     }
 }
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,18 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
+    public static PlayerManager Instance;
+    [SerializeField] private Player[] players;
+    [SerializeField] bool firstPlayerActive = true;
+
+    [SerializeField] int health = 5;
+
+    private float speed = 10;
+    private bool isMoving;
+    private Vector2 moveInput;
+    private Quaternion q;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instance = this;
+        q = Quaternion.AngleAxis(-45f, Vector3.up);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (!isMoving) return;
+        players[firstPlayerActive ? 0 : 1].Movement(speed * (q * new Vector3(moveInput.x, 0f, moveInput.y)));
+
+        if (firstPlayerActive) {
+            players[1].transform.position = players[0].transform.position + new Vector3(100, 0, 0);
+        } else {
+            players[0].transform.position = players[1].transform.position - new Vector3(100, 0, 0);
+        }
     }
+
+    void Toggle() {
+        if (health <= 0) return;
+        if (firstPlayerActive) {
+            players[1].transform.position = players[0].transform.position + new Vector3(100, 0, 0);
+        } else {
+            players[0].transform.position = players[1].transform.position - new Vector3(100, 0, 0);
+        }
+        if (players[firstPlayerActive ? 1 : 0].CheckCollision()) return;
+
+        players[firstPlayerActive ? 0 : 1].TogglePlayer(false);
+        players[firstPlayerActive ? 0 : 1].cam.gameObject.SetActive(false);
+
+        players[firstPlayerActive ? 1 : 0].TogglePlayer(true);
+        players[firstPlayerActive ? 1 : 0].cam.gameObject.SetActive(true);
+
+        firstPlayerActive = !firstPlayerActive;
+    }
+
+    public void OnMove(InputValue value) {
+        Debug.Log("Move!");
+        moveInput = value.Get<Vector2>();
+        isMoving = moveInput != Vector2.zero;
+    }
+
+    public void OnJump(InputValue value) {
+        Debug.Log("Jump!");
+        Toggle();
+    }
+
+
 }
