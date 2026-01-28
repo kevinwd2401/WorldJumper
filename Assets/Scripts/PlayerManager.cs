edfb805fee822268c5906f457586b94d8ab96a03
KevinDu10 <kevinwd2401@gmail.com>
2026-01-27 23:58:11 -0500

    World Jump improved

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
 {
     public static PlayerManager Instance;
+    public Slider jumpBar;
     [SerializeField] private Player[] players;
     [SerializeField] bool firstPlayerActive = true;
 
@@ -33,13 +35,23 @@ public class PlayerManager : MonoBehaviour
         if (holdingJump)
         {
             jumpHoldTime += Time.deltaTime;
+            jumpBar.value = jumpHoldTime;
+            if (jumpHoldTime > 1.0f) {
+                if (Toggle()) {
+                    Debug.Log("World Jump!");
+                } else {Debug.Log("Jump Failed!");}
+
+                holdingJump = false;
+                jumpHoldTime = 0;
+                jumpBar.value = 0;
+            }
         }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (!isMoving) return;
+        if (!isMoving || holdingJump) return;
         players[firstPlayerActive ? 0 : 1].Movement(speed * (q * new Vector3(moveInput.x, 0f, moveInput.y)));
 
         if (firstPlayerActive) {
@@ -49,14 +61,10 @@ public class PlayerManager : MonoBehaviour
         }
     }
 
-    void Toggle() {
-        if (health <= 0 || PeakCoroutine != null) return;
-        if (firstPlayerActive) {
-            players[1].transform.position = players[0].transform.position + new Vector3(100, 0, 0);
-        } else {
-            players[0].transform.position = players[1].transform.position - new Vector3(100, 0, 0);
-        }
-        if (players[firstPlayerActive ? 1 : 0].CheckCollision()) return;
+    bool Toggle() {
+        if (health <= 0 || PeakCoroutine != null) return false;
+        realigPos();
+        if (players[firstPlayerActive ? 1 : 0].CheckCollision()) return false;
 
         players[firstPlayerActive ? 0 : 1].TogglePlayer(false);
         players[firstPlayerActive ? 0 : 1].cam.gameObject.SetActive(false);
@@ -65,9 +73,12 @@ public class PlayerManager : MonoBehaviour
         players[firstPlayerActive ? 1 : 0].cam.gameObject.SetActive(true);
 
         firstPlayerActive = !firstPlayerActive;
+        return true;
     }
     
     private IEnumerator PeakCor() {
+        realigPos();
+
         players[firstPlayerActive ? 0 : 1].cam.gameObject.SetActive(false);
         players[firstPlayerActive ? 1 : 0].cam.gameObject.SetActive(true);
         yield return new WaitForSeconds(0.4f);
@@ -86,6 +97,14 @@ public class PlayerManager : MonoBehaviour
         return players[firstPlayerActive ? 0 : 1].transform.position;
     }
 
+    private void realigPos() {
+        if (firstPlayerActive) {
+            players[1].transform.position = players[0].transform.position + new Vector3(100, 0, 0);
+        } else {
+            players[0].transform.position = players[1].transform.position - new Vector3(100, 0, 0);
+        }
+    }
+
     public void OnMove(InputValue value) {
         moveInput = value.Get<Vector2>();
         isMoving = moveInput != Vector2.zero;
@@ -100,18 +119,14 @@ public class PlayerManager : MonoBehaviour
         }
         else
         {
-            if (jumpHoldTime >= 1.0f)
-            {
-                Debug.Log("World Jump!");
-                Toggle();
-            }
-            else if (PeakCoroutine == null)
+            if (holdingJump && jumpHoldTime < 1.0f && PeakCoroutine == null)
             {
                 Debug.Log("World Peak!");
                 PeakCoroutine = StartCoroutine(PeakCor());
             }
 
             holdingJump = false;
+            jumpBar.value = 0;
         }
     }
 
